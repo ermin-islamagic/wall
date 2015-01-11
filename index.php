038944bc188da6810ac87626ac4a7d36942af24b
Ermin <ermin@islamagic.com>
2015-01-11 20:43:12 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ermin Islamagic 
+ * Web: http://islamagic.com/
+ * Date: 11.1.2015
+ * Time: 16:41
+ */
+?>
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>My wall</title>
+</head>
+<body data-ng-app="wallApp">
+
+
+    <script src="assets/js/angular.min.js"></script>
+    <script src="assets/js/app.js"></script>
+</body>
+</html>
